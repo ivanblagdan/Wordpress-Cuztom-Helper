cfc5f4421213ac60966f357ff6ee0b1fb9404796
Ivan <ivan.blagdan@gmail.com>
2013-07-12 00:44:46 +0200

    checkbox bundleable

--- classes/fields/checkbox.class.php
+++ classes/fields/checkbox.class.php
@@ -4,9 +4,12 @@ if( ! defined( 'ABSPATH' ) ) exit;
 
 class Cuztom_Field_Checkbox extends Cuztom_Field
 {
+	var $_supports_repeatable 	= true;
+	var $_supports_bundle		= true;
+
 	function _output( $value, $object )
 	{
-		return '<div class="cuztom-checkbox-wrap"><input type="checkbox" name="cuztom[' . $this->id_name . ']" id="' . $this->id_name . '" ' . ( ! empty( $value ) ? checked( $value, 'on', false ) : checked( $this->default_value, 'on', false ) ) . ' class="cuztom-input" /></div>';
+		return '<div class="cuztom-checkbox-wrap"><input type="checkbox" name="cuztom' . $this->pre . '[' . $this->id_name . ']' . $this->after . '" id="' . $this->id_name . '" ' . ( ! empty( $value ) ? checked( $value, 'on', false ) : checked( $this->default_value, 'on', false ) ) . ' class="cuztom-input" /></div>';
 	}
 
 	function save( $post_id, $value, $meta_type )
